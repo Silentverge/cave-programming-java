50e85ad72e07ce32a12b44d12a6119877089e39b
Jim Spriggs <jrspriggs83@gmail.com>
2020-04-08 13:59:21 -0500

    initial commit

---
+++ src/main/java/jim/spriggs/Program.java
@@ -0,0 +1,7 @@
+package jim.spriggs;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/jim/spriggs/lessons/Day_1.java
@@ -0,0 +1,7 @@
+package jim.spriggs.lessons;
+
+public class Day_1 {
+    public static void main(String[] args) {
+        System.out.println("Day_1");
+    }
+}
